a36386d9e035de49ca8fdf6b4968659d29761b0c
kmseonhwa <seonhwakei@gmail.com>
2022-04-05 15:01:52 +0900

    Init: chatserver

---
+++ chatserver/src/test/java/com/stomptest1/chatserver/ChatserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stomptest1.chatserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
